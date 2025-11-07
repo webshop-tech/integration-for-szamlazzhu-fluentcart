ef652a4c2113d8e6a71da39b27ab19632a54a64b
Gabor Angyal <gabor.angyal@codesharp.dev>
2025-11-07 17:43:28 +0100

    set fluent-cart as required plugin

--- integration-for-szamlazzhu-fluentcart.php
+++ integration-for-szamlazzhu-fluentcart.php
@@ -9,6 +9,7 @@
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: integration-for-szamlazzhu-fluentcart
+ * Requires Plugins: fluent-cart
  */
 
 namespace SzamlazzHuFluentCart;
