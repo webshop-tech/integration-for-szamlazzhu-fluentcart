18884d8fd93cdd475a24f0a6f175cbddb51712bf
Gabor Angyal <gabor.angyal@codesharp.dev>
2026-02-12 15:37:32 +0100

    generate invoice on order status changed

--- includes/invoice-hooks.php
+++ includes/invoice-hooks.php
@@ -10,6 +10,15 @@ if ( ! defined( 'ABSPATH' ) ) exit;
     create_invoice($order);
 }, 10, 1);
 
+\add_action('fluent_cart/order_status_changed', function($data) {
+    $order = $data['order'];
+    $order_id = $order->id;
+    write_log($order_id, 'fluent_cart/order_status_changed', 'new status', $data['new_status']);
+    if ($data['new_status'] =='completed') {
+        create_invoice($order);
+    }
+}, 10, 1);
+
 \add_action('fluent_cart/payment_status_changed_to_paid', function($data) {
     $order = $data['order'];
     create_invoice($order);
